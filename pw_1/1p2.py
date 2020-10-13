afd636fcabf09113f15231181559c98f8e3929ae
machukhinktato <tarabrinmv@gmail.com>
2020-10-13 15:37:06 +0300

    done

---
+++ pw_1/1p2.py
@@ -0,0 +1,12 @@
+import requests
+from pprint import pprint
+from tokens import access_token
+import json
+
+response = requests.get(
+    f'https://api.vk.com/method/groups.get?v=5.52&access_token={access_token}')
+with open('lesson_1_t2.json', 'w', encoding='utf8') as f:
+    json.dump(response.text, f)
+
+# a = response.json()
+# pprint(a['response']['items'])

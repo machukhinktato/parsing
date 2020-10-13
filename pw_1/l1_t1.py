967dcd07cc18b91d1db5ff0d33d32dc687c5746f
machukhinktato <tarabrinmv@gmail.com>
2020-10-13 15:40:24 +0300

    done

--- pw_1/l1_t1.py
+++ pw_1/l1_t1.py
@@ -5,5 +5,5 @@ from pprint import pprint
 
 web = 'https://api.github.com/users/machukhinktato/repos'
 response = requests.get(web)
-with open('lesson_1.json', 'w', encoding='utf8') as f:
+with open('lesson_1_t1.json', 'w', encoding='utf8') as f:
     json.dump(response.text , f)
--- pw_1/l1_t2.py
+++ pw_1/l1_t2.py
@@ -7,6 +7,7 @@ response = requests.get(
     f'https://api.vk.com/method/groups.get?v=5.52&access_token={access_token}')
 with open('lesson_1_t2.json', 'w', encoding='utf8') as f:
     json.dump(response.text, f)
-
+response = json.loads(response.text)
+print(response['response']['items'])
 # a = response.json()
 # pprint(a['response']['items'])

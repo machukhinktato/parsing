66fc7da5c5239f3e1f5dd44ab2281d67559c5273
machukhinktato <tarabrinmv@gmail.com>
2020-10-14 16:10:42 +0300

    done

--- pw_1/lesson_1_t1.py
+++ pw_1/lesson_1_t1.py
@@ -9,7 +9,7 @@ web = 'https://api.github.com/user/repos'
 response = requests.get(web, auth=(user, psw))
 response = response.json()
 with open('lesson_1_t1.json', 'w', encoding='utf8') as f:
-    json.dump(response, f)
+    json.dump(response, f, indent=2) #indent for beauty code
 
 for repo in response:
     print(f'{repo["name"]} - {repo["description"]}')

4017e04ca9f3a17984c1b9b36ba2236fcc2ade3a
machukhinktato <tarabrinmv@gmail.com>
2020-10-14 17:52:11 +0300

    done

---
+++ pw_2/task02.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup as bs
+from pprint import pprint
+import json
+import requests
+
+# https://hh.ru/search/vacancy?clusters=true&area=1&enable_snippets=true&salary=&st=searchVacancy&text=Python
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/86.0.4240.75 Safari/537.36',
+}
+
+params = {
+
+}
+
+resource = 'https://hh.ru'
+req_params = '/search/vacancy?area=1&fromSearchLine=true&st=searchVacancy&text=Python&from=suggest_post'
+response = requests.get(resource + req_params, headers=headers)
+dom = bs(response.text, 'html.parser')
+pprint(dom)

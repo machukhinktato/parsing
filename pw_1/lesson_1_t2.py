e0c860c5221563b2bb2d9ef1097e42bbbdc9f443
machukhinktato <tarabrinmv@gmail.com>
2020-10-14 16:37:52 +0300

    done

--- pw_1/lesson_1_t2.py
+++ pw_1/lesson_1_t2.py
@@ -5,7 +5,8 @@ import json
 
 response = requests.get(
     f'https://api.vk.com/method/groups.get?v=5.52&access_token={access_token}')
+response = response.json()
 with open('lesson_1_t2.json', 'w', encoding='utf8') as f:
-    json.dump(response.text, f)
-response = json.loads(response.text)
-print(response['response']['items'])
+    json.dump(response, f, indent=2)
+
+pprint(response['response']['items'])

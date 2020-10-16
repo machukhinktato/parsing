5d6a09aacf2d8ccdfaa70ca2ce93c894851c48fd
machukhinktato <tarabrinmv@gmail.com>
2020-10-16 16:45:05 +0300

    done

--- pw_2/task02.py
+++ pw_2/task02.py
@@ -14,6 +14,21 @@ def int_maker(str_value):
     return int(result)
 
 
+def value_delimitter(compensation):
+    first_val, second_val = None, None
+    for result in compensation:
+        if '-' in result:
+            result = result.split('-')
+            first_val, second_val = int_maker(result[0]), int_maker(result[1][:-4])
+        elif 'от' in result:
+            first_val, second_val = int_maker(result[3:-4]), None
+        elif 'до' in result:
+            first_val, second_val = None, int_maker(result[3:-4])
+        else:
+            continue
+    return first_val, second_val
+
+
 # https://hh.ru/search/vacancy?clusters=true&area=1&enable_snippets=true&salary=&st=searchVacancy&text=Python
 
 # vacancy_pick = input('please, enter vacancy name: ').lower()
@@ -46,62 +61,70 @@ for val in data_list:
     vacancy.append(val.find('a', {'data-qa': 'vacancy-serp__vacancy-title'}).text)
     compensation = val.findAll(attrs={'data-qa': 'vacancy-serp__vacancy-compensation'})
     # print(compensation)
+    start, end, unit = None, None, None
+    all_cats = []
     if compensation:
-        start, end, unit = None, None, None
         for offered_sum in compensation:
-            all_cats = []
             # start, end, unit = None, None, None
             if 'руб' in offered_sum.text.lower():
                 unit = 'руб'
+                start, end = value_delimitter(offered_sum)
                 # print(offered_sum.text)
-                for result in offered_sum:
-                    if '-' in result:
-                        result = result.split('-')
-                        # for loop in range(len(result)):
-                        start, end = int_maker(result[0]), int_maker(result[1][:-4])
-                        # print(result)
-                        # print(f'{start}, {end}, {unit}')
-                    elif 'от' in result:
-                        start, end = int_maker(result[3:-4]), None
-                        # print(f'{start}, {end}')
-                    elif 'до' in result:
-                        start, end = None, int_maker(result[3:-4])
-                        # start, end = None
+                # for result in offered_sum:
+                #     if '-' in result:
+                #         result = result.split('-')
+                # for loop in range(len(result)):
+                # start, end = int_maker(result[0]), int_maker(result[1][:-4])
+                # print(result)
+                # print(f'{start}, {end}, {unit}')
+                # elif 'от' in result:
+                #     start, end = int_maker(result[3:-4]), None
+                # print(f'{start}, {end}')
+                # elif 'до' in result:
+                #     start, end = None, int_maker(result[3:-4])
+                # start, end = None
                 all_cats.append(start)
                 all_cats.append(end)
                 all_cats.append(unit)
                 salary.append(all_cats)
-            elif 'usd' in result:
-
+            elif 'usd' in offered_sum.text.lower():
                 unit = 'usd'
+                start, end = value_delimitter(offered_sum)
                 all_cats.append(start)
                 all_cats.append(end)
                 all_cats.append(unit)
                 salary.append(all_cats)
             else:
+                unit = 'eur'
+                start, end = value_delimitter(offered_sum)
                 all_cats.append(start)
                 all_cats.append(end)
                 all_cats.append(unit)
                 salary.append(all_cats)
-            # salary.append(offered_sum.text[-4:-1])
     else:
-
         all_cats.append(start)
         all_cats.append(end)
         all_cats.append(unit)
         salary.append(all_cats)
-else:
-    salary.append(None)
+        # salary.append(offered_sum.text[-4:-1])
+    # else:
+    #
+    #     all_cats.append(start)
+    #     all_cats.append(end)
+    #     all_cats.append(unit)
+    #     salary.append(all_cats)
+# else:
+#     salary.append(None)
 
 pprint(len(salary))
 pprint(len(links))
 pprint(len(vacancy))
-mega_list = {}
-for i in range(len(vacancy)):
-    mega_list.setdefault(vacancy[i], [salary[i], links[i]])
-    # mega_list.append(vacancy[i])
-    # mega_list.append(links[i])
-    # mega_list.append(salary[i])
+# mega_list = {}
+# for i in range(len(vacancy)):
+#     mega_list.setdefault(vacancy[i], [salary[i], links[i]])
+# mega_list.append(vacancy[i])
+# mega_list.append(links[i])
+# mega_list.append(salary[i])
 
 # pprint(mega_list)
 

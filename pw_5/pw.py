6e9afa4e4369346f497e57b4e389d8befdee32ca
machukhinktato <tarabrinmv@gmail.com>
2020-11-03 16:55:06 +0300

    done

--- pw_5/pw.py
+++ pw_5/pw.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
+import time
 
 chrome_options = Options()
 chrome_options.add_argument('start-maximized')
@@ -8,10 +9,14 @@ chrome_options.add_argument('start-maximized')
 chrome = webdriver.Chrome(options=chrome_options)
 
 chrome.get('https://mail.ru/')
-log_button = chrome.find_element_by_id('PH_authLink')
-log_button.click()
-# login = driver.find_element_by_id('PH_authLink')
-# login.send_keys('study.ai_172@mail.ru')
+# log_button = chrome.find_element_by_id('PH_authLink')
+# log_button.click()
+
+login = chrome.find_element_by_id('mailbox:login-input')
+login.send_keys('study.ai_172@mail.ru')
+time.sleep(1)
+next_button = chrome.find_element_by_id('mailbox:submit-button')
+next_button.click()
 #
 # passw = driver.find_element_by_id('user_password')
 # passw.send_keys('Password172')

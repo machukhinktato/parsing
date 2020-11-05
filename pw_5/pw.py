e42d6b7988d0a4a8a5f3b53f5b361aa8f3d7dd8d
machukhinktato <tarabrinmv@gmail.com>
2020-11-05 17:22:50 +0300

    done

--- pw_5/pw.py
+++ pw_5/pw.py
@@ -27,86 +27,68 @@ psw = WebDriverWait(chrome, 20).until(
 psw.send_keys(PSW)
 psw.send_keys(Keys.ENTER)
 
-
-# side_bar = WebDriverWait(chrome, 20).until(
-#     EC.visibility_of_element_located((By.CLASS_NAME,'sidebar__menu-ico'))
-# )
-# side_bar.click()
-
 inbox_element = WebDriverWait(chrome, 20).until(
     EC.visibility_of_element_located((By.CLASS_NAME, 'nav__item_active'))
-    # EC.visibility_of_element_located((By.LINK_TEXT, '/inbox/'))
 )
+# print(dir(inbox_element))
+# print(inbox_element.text)
 
-
-banana = chrome.find_elements_by_xpath('//a[contains(title, "Входящие")]')
-# title = inbox_element.get_attribute('data-title')
-print(banana)
-print(type(banana))
-print(dir(banana))
-# html = chrome.find_element_by_tag_name('html')
-# for i in range(5):
-list_of_links = []
-# html.send_keys(Keys.PAGE_DOWN)
-# for i in range(25):
-                        # banana = True
-                        # while banana:
-                        #     read_status_elem = WebDriverWait(chrome, 10).until(
-                        #         EC.presence_of_element_located((By.CLASS_NAME, 'llc__read-status')))
-                        #     actions = ActionChains(chrome)
-                        #     letters = chrome.find_elements_by_class_name('js-letter-list-item')
-                        #     for itm in letters:
-                        #         link = itm.get_attribute('href')
-                        #         list_of_links.append(link)
-                        #     try:
-                        #         actions.move_to_element(letters[-1])
-                        #         actions.perform()
-                        #     except:
-                        #         banana = False
-                        #         break
-    # finally:
-    #     break
-
-# paginator = chrome.find_element_by_class_name('paginator-container__block')
-# print(paginator)
 url_list = chrome.find_elements_by_class_name('js-letter-list-item')
-list_of_links = []
+
 urls = [url.get_attribute('href') for url in url_list]
 chrome.get(urls[0])
-time.sleep(1)
+emails = []
 while True:
-    # down_button = chrome.find_element_by_xpath('//span[@data-title="Следующее" or @title = "Следующее"]')
-    down_button = chrome.find_element_by_class_name('button2_arrow-down')
+    send_info = WebDriverWait(chrome, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'letter__author')))
+    try:
+        sender = send_info.find_element_by_class_name('letter-contact').text
+    except:
+        sender = None
+    try:
+        when_sended = send_info.find_element_by_class_name('letter__date').text
+    except:
+        when_sended = None
+    header = WebDriverWait(chrome, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, 'h2'))).text
+    try:
+        body = chrome.find_element_by_class_name('letter-body').text
+    except:
+        body = None
+    emails.append({'Sender': sender, 'When sended': when_sended, 'Header': header, 'Body': body})
+    # print(email)
+    # print(f"Обработана ссылка: {a}")
+# except Exception as e:
+# print(f'something going wrong\n {e}')
+
+    down_button = WebDriverWait(chrome, 20).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, 'button2_arrow-down'))
+    )
+
     if not down_button.get_attribute('disabled'):
         down_button.click()
     else:
         break
-# button2__wrapper
-# Keys.LEFT_CONTROL + Keys.PAGE_DOWN
-# next_button = WebDriverWait(chrome, 20).until(
-#     EC.visibility_of_element_located((By.CLASS_NAME, 'ico ico_16-arrow-down ico_size_s'))
-# )
-# print(next_button)
-# while True:
-    # next_button.send_keys(Keys.LEFT_CONTROL + Keys.PAGE_DOWN)
-# next_button.click()
-emails = []
-try:
-    for a in list_of_links:
-        chrome.get(a)
-        letter_author_wrapper = WebDriverWait(chrome, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'letter__author'))
-        )
-        email = {
-            'letter_author': letter_author_wrapper.find_element_by_class_name('letter-contact').get_attribute('title'),
-            'letter_date': letter_author_wrapper.find_element_by_class_name('letter__date').text,
-            'letter_title': chrome.find_element_by_class_name('thread__subject').text,
-            'letter_body': chrome.find_element_by_class_name('letter-body').text
-        }
-        emails.append(email)
-        print(f"Обработана ссылка: {a}")
-except Exception as e:
-    print(f'something going wrong\n {e}')
+
+print(emails)
+# emails = []
+# try:
+#     for a in list_of_links:
+#         chrome.get(a)
+#         letter_author_wrapper = WebDriverWait(chrome, 20).until(
+#             EC.presence_of_element_located((By.CLASS_NAME, 'letter__author'))
+#         )
+#
+#         email = {
+#             'letter_author': letter_author_wrapper.find_element_by_class_name('letter-contact').get_attribute('title'),
+#             'letter_date': letter_author_wrapper.find_element_by_class_name('letter__date').text,
+#             'letter_title': chrome.find_element_by_class_name('thread__subject').text,
+#             'letter_body': chrome.find_element_by_class_name('letter-body').text
+#         }
+#         emails.append(email)
+        # print(f"Обработана ссылка: {a}")
+# except Exception as e:
+#     print(f'something going wrong\n {e}')
 # finally:
     # chrome.close()
     # url_list = chrome.find_elements_by_class_name('js-letter-list-item')

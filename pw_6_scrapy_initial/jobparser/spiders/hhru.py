f1fcd65b4012b2931b8d2fb67b1883dab243d07d
machukhinktato <tarabrinmv@gmail.com>
2020-11-09 19:53:36 +0300

    done

--- pw_6_scrapy_initial/jobparser/spiders/hhru.py
+++ pw_6_scrapy_initial/jobparser/spiders/hhru.py
@@ -1,10 +1,14 @@
 import scrapy
+from scrapy.http import HtmlResponse
+from pw_6_scrapy_initial.jobparser.items import JobparserItem
 
 
 class HhruSpider(scrapy.Spider):
     name = 'hhru'
     allowed_domains = ['hh.ru']
-    start_urls = ['http://hh.ru/']
+    start_urls = ['http://hh.ru/' + target]
+    target = 'search/vacancy?area=1&fromSearchLine=true&st=searchVacancy&text=Python&from=suggest_post'
 
-    def parse(self, response):
-        pass
+    def parse(self, response:HtmlResponse):
+        links = response.xpath("//a[@class='bloko-link HH-LinkModifier']/@href").extract()
+        next_page = response.xpath("//a[contains(@class,'HH-Pager-Controls-Next')]/@href").extract_first()

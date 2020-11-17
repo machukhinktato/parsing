78c78db0ed9e02c36908ee67f488496921f8e123
machukhinktato <tarabrinmv@gmail.com>
2020-11-17 15:06:23 +0300

    done

--- pw_6_scrapy_initial/jobparser/spiders/hhru.py
+++ pw_6_scrapy_initial/jobparser/spiders/hhru.py
@@ -7,7 +7,8 @@ class HhruSpider(scrapy.Spider):
     name = 'hhru'
     allowed_domains = ['hh.ru']
     start_urls = ['http://hh.ru/' + target]
-    target = 'search/vacancy?area=1&fromSearchLine=true&st=searchVacancy&text=Python&from=suggest_post'
+    # target = 'search/vacancy?area=1&fromSearchLine=true&st=searchVacancy&text=Python&from=suggest_post'
+    target = 'search/vacancy?clusters=true&enable_snippets=true&text=python&L_save_area=true&area=1&from=cluster_area&showClusters=true'
 
     def parse(self, response:HtmlResponse):
         urls = response.xpath("//a[@class='bloko-link HH-LinkModifier']/@href").extract()
@@ -20,5 +21,8 @@ class HhruSpider(scrapy.Spider):
     def vacancy_parse(self, response:HtmlResponse):
         name = response.xpath("//h1/text()").extract_first()
         salary = response.xpath("//p/span[@data-qa='bloko-header-2']/text()").extract()
+        # link = response.xpath()
+        # posted =
+
 
         yield JobparserItem(name=name, salary=salary)
